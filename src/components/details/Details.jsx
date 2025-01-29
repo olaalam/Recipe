06a2cfd5fc9a8499a01a854bf708c7edcf364403
olaalam <cds.olaallam382330@alexu.edu.eg>
2025-01-29 04:59:39 +0200

    sec commit

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Loading from "../Loading";
-import styles from "./Details.module.scss";
+import styles from "./details.module.scss";
 import Notfound from "../Notfound";
 
 export default function Details() {
